2d2374d7a824f4b7c98c450b582d50297900c95f
Fabius42 <33182716+Fabius42@users.noreply.github.com>
2021-01-10 16:02:12 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import plotly.express as px
 import numpy as np
 import dash_core_components as dcc
 import plotly.graph_objects as go
+import yfinance as yf
 
 
 # get stylesheetd
@@ -25,11 +26,104 @@ external_stylesheets = [
 app = dash.Dash(__name__, external_stylesheets = external_stylesheets)
 
 # import ticks
-tick_list = pd.read_csv('C:/Users/hicom/Documents/Side Projects/AS_hackathon_8.1/esg-scores.csv')
+tick_list = pd.read_csv('C:/Users/fdiet/Desktop/portfolio-impact-assessment-main/esg-scores.csv')
 
 # get tick list for dropdown
 available_stocks = tick_list['tick'].unique()
 
+# user portfolio consisting of several stock tickers
+user_portfolio = ['AMZN', 'TSLA', 'GOOGL']
+
+
+def compute_portfolio_performance(portfolio):
+    # get closing data
+    df = data.DataReader(portfolio, 'yahoo', start='2015/01/01', end='2020/12/31')
+    df = df['Adj Close']
+    
+    # Yearly returns for individual companies
+    ind_er = df.resample('Y').last().pct_change().mean()
+
+    # Volatility is given by the annual standard deviation. We multiply by 250 because there are 250 trading days/year.
+    ann_sd = df.pct_change().apply(lambda x: np.log(1+x)).std().apply(lambda x: x*np.sqrt(250))
+    
+    assets = pd.concat([ind_er, ann_sd], axis=1) # Creating a table for visualising returns and volatility of assets
+    assets.columns = ['Returns', 'Volatility']
+    assets['ESG'] = list(tick_list[tick_list.tick.isin(portfolio)].esg)
+
+    '''
+    p_ret = [] # Define an empty array for portfolio returns
+    p_vol = [] # Define an empty array for portfolio volatility
+    p_weights = [] # Define an empty array for asset weights
+    '''
+
+    num_assets = len(df.columns)
+
+    cov_matrix = df.pct_change().apply(lambda x: np.log(1+x)).cov()
+
+    weights = np.random.random(num_assets)
+    weights = weights/np.sum(weights)
+    #p_weights.append(weights)
+    returns = np.dot(weights, ind_er) # Returns are the product of individual expected returns of asset and its 
+                                        # weights 
+    #p_ret.append(returns)
+    var = cov_matrix.mul(weights, axis=0).mul(weights, axis=1).sum().sum()# Portfolio Variance
+    sd = np.sqrt(var) # Daily standard deviation
+    ann_sd = sd*np.sqrt(250) # Annual standard deviation = volatility
+    #p_vol.append(ann_sd)
+
+    esg = np.dot(weights, assets['ESG'])
+    rf = 0.01
+    idx_sharp = ((returns-rf)/ann_sd)
+
+    result_list = [portfolio, idx_sharp, esg]
+    return result_list
+
+
+# get list of only ticker symbols
+company_list = pd.read_csv('C:/Users/fdiet/Desktop/portfolio-impact-assessment-main/esg-scores.csv').tick
+
+def get_portfolio_variations(portfolio, company_list):
+    # get list of companies which are not in user portfolio
+    non_user_companies = []
+    for company in company_list:
+        if company in portfolio:
+            continue
+        non_user_companies.append(company)
+    all_possible_portfolios = []
+    for i in range(len(portfolio)):
+        for j in range(len(non_user_companies)):
+            new_portfolio = portfolio[:i] + portfolio[i+1:] + [non_user_companies[j]]
+            all_possible_portfolios.append([new_portfolio])
+    return all_possible_portfolios
+
+
+# compute_portfolio_performance(user_portfolio)
+
+possible_portfolios = get_portfolio_variations(user_portfolio, company_list)
+
+print(len(possible_portfolios))
+
+current_best_esg = 99
+current_best_sharp = -99
+
+good_picks = []
+counter = 0
+for p in possible_portfolios:
+    counter += 1
+    print(str(counter) + ' of ' + str(len(possible_portfolios)))
+    # look at result
+    result = compute_portfolio_performance(p[0])
+    result_sharp = result[1]
+    result_esg = result[2]
+    # if result has higher sharp AND lower esg add it to good picks
+    if result_sharp >= current_best_sharp and result_esg <= current_best_esg:
+        good_picks.append(result)
+        current_best_sharp = result_sharp
+        current_best_esg = result_esg
+        print(good_picks)
+
+print('good picks are:')
+print(good_picks)
 
 def calculate_frontier(ticks):
     # get stock closing data 
@@ -52,7 +146,7 @@ def calculate_frontier(ticks):
     p_esg = []
     
     num_assets = len(df.columns)
-    num_portfolios = 10
+    num_portfolios = 100
     
     cov_matrix = df.pct_change().apply(lambda x: np.log(1+x)).cov()
     
@@ -169,6 +263,8 @@ def update_scatter(stock_drop):
     #print(stock_drop)
 
 
-  
+
+'''
 if __name__ == '__main__':
     app.run_server(debug=True)
+'''
